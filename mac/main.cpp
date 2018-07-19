029373a3668568dd28dfc6cff9afd18a3a9f5369
Wang Eric <wangeric@wangs-macbook-pro.local>
2018-07-19 16:42:40 +0800

    Initial Commit

---
+++ mac/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mac
+//
+//  Created by Wang Eric on 19/7/18.
+//  Copyright © 2018 Wang Eric. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
